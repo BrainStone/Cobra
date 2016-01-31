1373c685499ca7c209fc0148ce03c1d0fc52f387
JackWhite20 <phil.westf@web.de>
2016-01-31 01:01:55 +0100

    Added basic classes for filtering specific headers or locations.

---
+++ server/src/main/java/de/jackwhite20/cobra/server/filter/FilteredRequest.java
@@ -0,0 +1,64 @@
+/*
+ * Copyright (c) 2016 "JackWhite20"
+ *
+ * This file is part of Cobra.
+ *
+ * Cobra is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package de.jackwhite20.cobra.server.filter;
+
+import de.jackwhite20.cobra.server.http.HTTPRequest;
+import de.jackwhite20.cobra.server.http.HTTPResponse;
+import de.jackwhite20.cobra.server.http.RequestMethod;
+
+/**
+ * Created by JackWhite20 on 31.01.2016.
+ */
+public class FilteredRequest {
+
+    private HTTPRequest request;
+
+    private HTTPResponse response;
+
+    public FilteredRequest(HTTPRequest request) {
+
+        this.request = request;
+    }
+
+    public void abortWith(HTTPResponse response) {
+
+        this.response = response;
+    }
+
+    public RequestMethod method() {
+
+        return request.method();
+    }
+
+    public String location() {
+
+        return request.location();
+    }
+
+    public String header(String key) {
+
+        return request.header(key);
+    }
+
+    public HTTPResponse response() {
+
+        return response;
+    }
+}
